ca473be4cb86f2d6b3620e22fa28fafe5087c551
cs4alhaider <cs.alhaider@gmail.com>
2024-12-28 17:43:34 +0300

    uncommenting the main function

--- main.py
+++ main.py
@@ -112,7 +112,6 @@ def automate_resource_creation():
 
 if __name__ == "__main__":
     try:
-        # automate_resource_creation()
-        print("Hello World")
+        automate_resource_creation()
     except Exception as e:
         print(f"Error: {e}")
